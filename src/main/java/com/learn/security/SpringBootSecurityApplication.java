a606d967ebb7bb1738d1b75214d3813d305aae8c
wangqinggang <wqinggang@foxmail.com>
2022-10-11 23:26:29 +0800

    ////

--- src/main/java/com/learn/security/SpringBootSecurityApplication.java
+++ src/main/java/com/learn/security/SpringBootSecurityApplication.java
@@ -1,15 +1,15 @@
-package com.learn.wqgstart;
+package com.learn.security;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "com/learn/wqgstart/mapper")
-public class WqgStartApplication {
+@MapperScan(value = "com/learn/security/mapper")
+public class SpringBootSecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WqgStartApplication.class, args);
+        SpringApplication.run(SpringBootSecurityApplication.class, args);
     }
 
 }
